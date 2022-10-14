eb32013b956132439064c9ef2685ff10a7cb79f5
jwy <>
2022-10-14 09:25:01 +0900

    dir 수정

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+//앱셋팅
+app.use(express.static(`${__dirname}/src/public`));
+
 app.get('/', (req, res) => {
   res.send('root');
 });
--- src/bin/www.js
+++
@@ -1,5 +0,0 @@
-const app = require("../app");
-
-app.listen(process.env.PORT || 8081, () => {
-  console.log('서버가동');
-});
